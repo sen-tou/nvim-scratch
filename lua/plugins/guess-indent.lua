77bae607166d48021800e0192e0ce45560fc5945
sen-tou <stvbyr@users.noreply.github.com>
2024-05-18 13:12:01 +0200

    configure indenting with editorconfig

--- lua/plugins/guess-indent.lua
+++ lua/plugins/guess-indent.lua
@@ -1,4 +1,9 @@
 return {
-	{ 'nmac427/guess-indent.nvim',
-	opts = {} , event = 'BufEnter'}
+    { 'nmac427/guess-indent.nvim',
+    opts = {
+        -- respect indetation even if there is a .editorconfig
+        -- good for external code that shouldn't be reindented
+        override_editorconfig = true
+    } , event = 'BufEnter'}
 }
+
--- lua/plugins/opts.lua
+++ lua/plugins/opts.lua
@@ -2,6 +2,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.g.editorconfig = true
+
 -- some options
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -11,5 +13,11 @@ vim.opt.clipboard = 'unnamedplus'
 
 -- scrolloff to the top and bottom of the buffer
 vim.opt.scrolloff = 10
+
+-- default tabs/spaces config (will be overriden by detection plugin as needed)
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
 
 return {}
