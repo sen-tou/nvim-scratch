59855c168b44a0ef00c5b419a846ff81487fc087
sen-tou <29461308+sen-tou@users.noreply.github.com>
2024-11-01 15:56:57 +0100

    rm guess indent as it will be replaced by another plugin

--- lua/plugins/guess-indent.lua
+++
@@ -1,9 +0,0 @@
-return {
-    { 'nmac427/guess-indent.nvim',
-    opts = {
-        -- respect indetation even if there is a .editorconfig
-        -- good for external code that shouldn't be reindented
-        override_editorconfig = true
-    } , event = 'BufEnter'}
-}
-
