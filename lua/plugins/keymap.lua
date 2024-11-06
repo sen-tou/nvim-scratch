e07d83f9430b26e9fc568189f98d30a6cc63d350
sen-tou <29461308+sen-tou@users.noreply.github.com>
2024-11-06 14:15:24 +0100

    add paste/delete keys without affection buffers

--- lua/plugins/keymap.lua
+++ lua/plugins/keymap.lua
@@ -6,14 +6,17 @@ vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move window right' })
 
 -- better terminal commands
 -- |NOTE| this does not work in WSL2 and it seems not to be fixed by microsoft at this point
-vim.keymap.set('t', "<C-\\><C-n>", '<Leader><ESC>', { noremap = true, silent = true, desc = 'Exit terminal mode with ESC' })
+vim.keymap.set('t', '<C-\\><C-n>', '<Leader><ESC>', { noremap = true, silent = true, desc = 'Exit terminal mode with ESC' })
 
 vim.keymap.set('n', '<Up>', ':<Up>', { desc = 'Switch through last command without typing' })
 
 vim.keymap.set('n', '<ESC>', '<cmd>nohlsearch<CR>', { desc = 'Remove search highlight' })
 
 -- stay in visual mode after indenting
-vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })
-vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
+vim.keymap.set('v', '>', '>gv', { noremap = true, silent = true })
+vim.keymap.set('v', '<', '<gv', { noremap = true, silent = true })
+
+vim.keymap.set({'n', 'v'}, '<leader>p', '"_p', { noremap = true, silent = true, desc = 'Paste without affecting the current register' })
+vim.keymap.set({'n', 'v'}, '<leader>d', '"_d', { noremap = true, silent = true, desc = 'Delete without affecting the current register' })
 
 return {}
