f0421b5cd3956b1bb5ab2defca36eb07e9f2dab4
sen-tou <29461308+sen-tou@users.noreply.github.com>
2024-11-09 13:22:14 +0100

    change theme to github theme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,16 +1,17 @@
 return {
-    'Shatur/neovim-ayu',
-    lazy = false,
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false,  -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-        vim.cmd.colorscheme 'ayu-dark'
+        require('github-theme').setup({})
 
-        local colors = require('ayu.colors')
-        colors.generate(true)
+        vim.cmd('colorscheme github_dark_default')
 
-        vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-        vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-        vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
-        vim.api.nvim_set_hl(0, 'LineNr', { fg = colors.foreground })
-        vim.api.nvim_set_hl(0, 'Visual', { bg = colors.selection_bg })
-    end
+        -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+        -- vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+        -- vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+        -- vim.api.nvim_set_hl(0, 'LineNr', { fg = colors.foreground })
+        -- vim.api.nvim_set_hl(0, 'Visual', { bg = colors.selection_bg })
+    end,
 }
