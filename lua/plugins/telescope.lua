2a77e55a56544dc190575df36ef55a98eb7eca25
sen-tou <29461308+sen-tou@users.noreply.github.com>
2024-11-20 13:16:44 +0100

    refactor telescope plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,11 @@
-return {
+local M = {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     event = 'VimEnter',
 
     dependencies = {
         'nvim-lua/plenary.nvim',
-        { -- Telescope FZF Native for faster sorting
+        {     -- Telescope FZF Native for faster sorting
             'nvim-telescope/telescope-fzf-native.nvim',
             build = 'make',
             cond = function()
@@ -13,40 +13,42 @@ return {
             end,
         },
         { 'nvim-telescope/telescope-ui-select.nvim' },
-        { 
-            'nvim-tree/nvim-web-devicons', 
-            enabled = vim.g.have_nerd_font  -- Requires a Nerd Font for pretty icons 
+        {
+            'nvim-tree/nvim-web-devicons',
+            enabled = vim.g.have_nerd_font     -- Requires a Nerd Font for pretty icons
         },
     },
+}
 
-    config = function()
-        local telescope = require('telescope')
-        local builtin = require('telescope.builtin')
+M.config = function()
+    local telescope = require('telescope')
+    local builtin = require('telescope.builtin')
 
-        telescope.setup {
-            defaults = {
-                file_ignore_patterns = {'node_modules', '.git', 'vendor', '.ssh'}
-            },
-            pickers = {
-                find_files = {
-                    hidden = true
-                }
-            },
-            extensions = {
-                ['ui-select'] = {
-                    require('telescope.themes').get_dropdown({})
-                }
+    telescope.setup {
+        defaults = {
+            file_ignore_patterns = { 'node_modules', '.git', 'vendor', '.ssh' }
+        },
+        pickers = {
+            find_files = {
+                hidden = true
+            }
+        },
+        extensions = {
+            ['ui-select'] = {
+                require('telescope.themes').get_dropdown({})
             }
         }
+    }
 
-        -- Load extensions
-        pcall(telescope.load_extension, 'fzf')
-        pcall(telescope.load_extension, 'ui-select')
+    -- Load extensions
+    pcall(telescope.load_extension, 'fzf')
+    pcall(telescope.load_extension, 'ui-select')
 
-        -- Keybindings
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader> ', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    end,
-}
+    -- Keybindings
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader> ', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+end
+
+return M
