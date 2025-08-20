8f68c5a388c7a76019756bceaa55c0de7d48dd51
sen-tou <29461308+sen-tou@users.noreply.github.com>
2025-08-20 13:45:18 +0200

    fix format shortcut

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,6 +3,7 @@ return {
     opts = {
         formatters_by_ft = {
             lua = { 'stylua' },
+            c = { 'clang-format' }
         },
         format_on_save = {
             -- These options will be passed to conform.format()
@@ -14,7 +15,7 @@ return {
         {
             '<leader>cf',
             function()
-                require('conform').format()
+                require('conform').format({ async = true, lsp_format = 'fallback' })
             end,
             desc = 'Conform: Format'
         },
